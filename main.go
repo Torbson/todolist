9fbfde6abd37be95370c36183ad25445f78107ad
Torbson <Thorson@rocketmail.com>
2021-05-03 15:39:32 +0200

    slight adjustments to .env and main.go

--- main.go
+++ main.go
@@ -15,13 +15,14 @@ var db *gorm.DB
 
 func connect_db(dsn string) {
 	// Connect PostgreSQL
+	log.Print("connect to Postgres ...")
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("failed to connect database")
 	}
-	log.Print("connected to postgres")
+	log.Print("successfully connected to Postgres")
 	// Auto migrate in case of schema / model changes
 	db.AutoMigrate(&Todo{}, &Task{})
 }
@@ -58,28 +59,4 @@ func main() {
 
 	// Start http server
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), muxRouter()))
-
-	// golang TLS config
-	/*
-		cfg := &tls.Config{
-			MinVersion:               tls.VersionTLS12,
-			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
-		}
-		// Create server
-		srv := &http.Server{
-			Addr:         ":8443",
-			Handler:      muxRouter(),
-			TLSConfig:    cfg,
-			TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
-		}
-		// Start https server
-		log.Fatal(srv.ListenAndServeTLS("cert.pem", "key.pem"))*/
-
 }
